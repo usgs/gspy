ca86cad42e2e0bcae951aaf78eabc8098ce4997c
Leon Foks <leonfoks@gmail.com>
2023-12-05 10:47:54 -0800

    update version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import find_packages, setup
 from distutils.command.sdist import sdist
 cmdclass={'sdist': sdist}
 
-__version__ = '1.1.0'
+__version__ = '1.1.1'
 
 setup(name='gspy',
     packages=find_packages(),
