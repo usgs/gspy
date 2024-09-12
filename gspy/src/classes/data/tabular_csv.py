3ffdb1053d3ae63de1c9f17871952dd0aedf19f9
Leon Foks <leonfoks@gmail.com>
2024-09-12 09:48:36 -0700

    Quick fix for missing method

--- gspy/src/classes/data/tabular_csv.py
+++ gspy/src/classes/data/tabular_csv.py
@@ -50,6 +50,40 @@ class Tabular_csv(Tabular):
         assert all([x in kwargs for x in ('long_name', 'standard_name', 'null_value', 'units')]), ValueError("Must have at least 'long_name', 'standard_name', 'null_value', 'units'")
         return kwargs
 
+    def write_metadata_template(self, filename="csv_md.yml"):
+        """Write metadata template for a CSV dataset
+
+        but we require extra attributes to honour the CF convention.
+        Additional dictionaries can be optionally added to document more information or
+        ancillary metadata relevant to the specific variables.
+
+        Raises
+        ------
+        Exception
+            Tells user to specify metadata file when instantiating
+
+        """
+
+        assert False, Exception("FIX MEEEEEEEE")
+
+        print("\nGenerating an empty metadata file template.\n")
+
+        out = {}
+        out["dataset_attrs"] = {
+            "content": "<summary statement of what the dataset contains>",
+            "comment": "<additional details or ancillary information>"
+            }
+
+        out["coordinates"] = {
+            "x" : "lat",
+            "y" : "lon",
+            "z" : "depth"
+            }
+
+        out["variable_metadata"] = {}
+
+        dump_metadata_to_file(out, filename)
+
     @staticmethod
     def _create_variable_metadata_template(filename, columns, template_filename=None, **kwargs):
         """Generates a template metadata file.
