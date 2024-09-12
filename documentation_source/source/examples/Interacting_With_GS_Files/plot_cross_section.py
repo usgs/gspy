55207ebb970a52784c79661f4d219a4d4c678de6
Leon Foks <leonfoks@gmail.com>
2024-09-12 11:54:57 -0700

    cleanup

--- documentation_source/source/examples/Interacting_With_GS_Files/plot_cross_section.py
+++ documentation_source/source/examples/Interacting_With_GS_Files/plot_cross_section.py
@@ -1,3 +1,9 @@
+"""
+Plotting Cross Sections
+-----------------------
+
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 from os.path import join
@@ -14,8 +20,9 @@ survey = Survey.open_netcdf(data_path + "Tempest.nc")
 
 tab = survey.tabular[1]
 
-fig, ax = plt.subplots(1)
-ax, pm, cb = survey.tabular[1].gs_tabular.plot_cross_section(line_number=212201, variable='conductivity', hang_from=None, axis='distance', ax=ax, equalize=True, log=10, cmap='jet', ylim=[-200, 100.0])
+plt.figure()
+ax, pm, cb = survey.tabular[1].gs_tabular.plot_cross_section(line_number=212201, variable='conductivity', hang_from=None, axis='distance', equalize=True, log=10, cmap='jet', ylim=[-200, 100.0])
 
-
+plt.figure()
+_, pm, cb = survey.tabular[1].gs_tabular.plot_cross_section(line_number=212201, variable='conductivity', hang_from='elevation', axis='distance', equalize=True, log=10, cmap='jet', ylim=[-200, 100.0])
 plt.show()
--- update_version.py
+++ update_version.py
@@ -4,6 +4,17 @@ Parser = argparse.ArgumentParser(description="update_version", formatter_class=a
 Parser.add_argument('version', help='version')
 args = Parser.parse_args()
 
+with open("documentation_source/source/conf.py", "r") as f:
+        lines = f.readlines()
+with open("documentation_source/source/conf.py", "w") as f:
+        for line in lines:
+            if "version =" in line:
+                f.write('version = "{}"\n'.format(args.version))
+            elif "release =" in line:
+                f.write('release = "{}"\n'.format(args.version))
+            else:
+                f.write(line)
+
 try:
     with open("setup.py", "r") as f:
         lines = f.readlines()
